cc2699e1b432244a97ac9c3c40f698756e7947c1
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-06-13 13:55:30 +0530

    initialized

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View className="bg-blue-400">
+      <Text className="text-red-500">index</Text>
+    </View>
+  );
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
